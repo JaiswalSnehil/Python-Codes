35bf20c36713831af6b00950b9224fbd4f60f588
Snehil Jaiswal <112891265+JaiswalSnehil@users.noreply.github.com>
2025-08-11 12:15:13 +0530

    Add files via upload

---
+++ Hashing Passwords.py
@@ -0,0 +1,10 @@
+import hashlib
+
+def hash_password(password):
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    return hashed_password
+
+# Example
+password = "my_secure_password"
+hashed_password = hash_password(password)
+print("Hashed password:", hashed_password)

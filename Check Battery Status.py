6d8975892108c0a87f9e5bff5db063fe75dd2cce
Snehil Jaiswal <112891265+JaiswalSnehil@users.noreply.github.com>
2025-08-27 09:49:53 +0530

    Add files via upload

---
+++ Check Battery Status.py
@@ -0,0 +1,17 @@
+import psutil
+
+battery = psutil.sensors_battery()
+
+if battery is not None:
+    print("Battery Percentage:", battery.percent, "%")
+
+    print("Power Plugged in:", battery.power_plugged)
+
+    def convertTime(seconds):
+        minutes, seconds = divmod(seconds, 60)
+        hours, minutes = divmod(minutes, 60)
+        return "%d:%02d:%02d" % (hours, minutes, seconds)
+    
+    print("Battery remaining Time:", convertTime(battery.secsleft))
+else:
+    print("No battery information available.")

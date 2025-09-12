44adede9fe93dfceeefb55ff94daa324f88e5069
Snehil Jaiswal <112891265+JaiswalSnehil@users.noreply.github.com>
2025-09-12 19:31:13 +0530

    Add files via upload

---
+++ Lists/Chunk a List.py
@@ -0,0 +1,9 @@
+# Split a list into evenly sized chunks
+
+def chunk(lst, n):
+    for i in range(0, len(lst), n):
+        yield lst[i:i+n]
+
+lst = [1, 2, 3, 4, 5, 6, 7, 8]
+chunks = list(chunk(lst, 3))
+print(chunks)

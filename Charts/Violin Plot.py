54b4844fbc45933a34319fc2fe0c0d9bff6319e6
Snehil Jaiswal <112891265+JaiswalSnehil@users.noreply.github.com>
2025-09-21 14:43:06 +0530

    Add files via upload

---
+++ Charts/Violin Plot.py
@@ -0,0 +1,11 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+data = [np.random.normal(0, std, 180) for std in range(1, 4)]
+
+sns.violinplot(data=data)
+plt.title('Violin Plot')
+plt.xlabel('Category')
+plt.ylabel('Values')
+plt.show()

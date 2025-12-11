642aa739006c64bb8ef44cdfd64c4b30472959b2
Snehil Jaiswal <112891265+JaiswalSnehil@users.noreply.github.com>
2025-12-11 21:43:06 +0530

    Add files via upload

---
+++ Applications/Interactive HTML Table.py
@@ -0,0 +1,14 @@
+import plotly.graph_objects as go
+
+data = dict(
+    Name=["Alice","Bob","Clare"],
+    Score=[88,92,85],
+    City=["NY","LA","TX"]
+)
+
+table = go.Figure(data=[go.Table(
+    header=dict(values=list(data.keys())),
+    cells=dict(values=list(data.values()))
+)])
+
+table.show()

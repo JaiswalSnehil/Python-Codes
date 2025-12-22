4cb29612fa23368ebeb7ae7543b420d13bb3a1b6
Snehil Jaiswal <112891265+JaiswalSnehil@users.noreply.github.com>
2025-12-22 16:16:34 +0530

    Delete Fetch Interest over Time.py

--- Fetch Interest over Time.py
+++
@@ -1,8 +0,0 @@
-from pytrends.request import TrendReq
-
-pytrends = TrendReq()
-keywords = ["Python", "Java", "C++"]
-pytrends.build_payload(keywords, timeframe= "today 12-m")
-data = pytrends.interest_over_time()
-
-print(data)
